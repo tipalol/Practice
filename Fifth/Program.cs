a5c892e1e1a01d2fbbd9f6ae1335ee90d0f6ee3c
sorokindmitrij <tipalol2@gmail.com>
2019-06-15 12:50:22 +0500

    Ended 5 work

--- Fifth/Program.cs
+++ Fifth/Program.cs
@@ -4,9 +4,43 @@ namespace Fifth
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var random = new Random();
+            const int rang = 9;
+            int[,] matrice = new int[rang,rang], result = new int[rang, rang];
+
+
+            Console.WriteLine("Исходная матрица:");
+            //Генерируем матрицу с помощью генератора случайных чисел
+            //и выводим матрицу на экран
+            for (int i = 0; i < rang; i++)
+            {
+                for (int j = 0; j < rang; j++)
+                {
+                    matrice[i, j] = random.Next(0,10);
+                    Console.Write(matrice[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //Формируем требуемую матрицу
+            for (int i = 0; i < rang; i++)
+            {
+                for (int j = 0; j < rang; j++)
+                    if (matrice[i, j] > matrice[i, i])
+                        result[i, j] = 1;
+                    else
+                        result[i, j] = 0;
+            }
+
+            Console.WriteLine("Полученная матрица:");
+            for (int i = 0; i < rang; i++)
+            {
+                for (int j = 0; j < rang; j++)
+                    Console.Write($"{result[i,j]} ");
+                Console.WriteLine();
+            }
         }
     }
 }
