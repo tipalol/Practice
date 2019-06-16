ea2c48daead7b625c6c4e75f68dfcb4ec3228488
sorokindmitrij <tipalol2@gmail.com>
2019-06-16 16:49:12 +0500

    Finnaly i've end it

---
+++ Eighth/Matrice.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Eleven;
+namespace Eighth
+{
+    public class Matrice
+    {
+        Menu menu = new Menu(null);
+        int[,] Elements { get; set; }
+        int Size {
+            get
+            {
+                return (int)Math.Sqrt(Elements.Length);
+            }
+        }
+        public int this[int index, int index2]
+        {
+            get
+            {
+                return Elements[index, index2];
+            }
+            set
+            {
+                Elements[index, index2] = value;
+            }
+        }
+        public void interchange(int i, int j)
+        {
+            for (int k = 0; k < Size; k++)
+            {
+                int temp = Elements[i - 1, k];
+                Elements[i - 1, k] = Elements[j - 1, k];
+                Elements[j - 1, k] = temp;
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                Console.WriteLine($"Строка номер {i+1}");
+                for (int j = 0; j < Size; j++)
+                    Console.Write($"{Elements[i,j]} ");
+                Console.WriteLine();
+            }
+        }
+        static bool IsIt(Matrice a, Matrice b)
+        {
+            bool result = true;
+            for (int i = 0; i < a.Size; i++)
+                for (int j = 0; j < a.Size; j++)
+                    if (a[i, j] != b[i, j])
+                        result = false;
+            return result;
+        }
+        
+        public static bool isIsomorph(Matrice a, Matrice b)
+        {
+            bool result = false;
+            int size = a.Size;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (IsIt(a,b))
+                    {
+                        result = true;
+                        return result;
+                    }
+                    a.interchange(i+1, j+1);
+                }
+                if (IsIt(a, b))
+                {
+                    result = true;
+                    return result;
+                }
+            }
+            return result;
+        }
+        public Matrice(int rang)
+        {
+            Elements = new int[rang, rang];
+            for (int i = 0; i < rang; i++)
+            {
+                for (int j = 0; j < rang; j++)
+                {
+                    Elements[i, j] = menu.GetInt($"элемент матрицы смежности в {i} строке, в {j} столбце");
+                }
+            }
+        }
+        public Matrice(int[,] matrice)
+        {
+            Elements = matrice;
+            
+        }
+    }
+}
--- Eighth/Program.cs
+++ Eighth/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Eleven;
 
 namespace Eighth
 {
@@ -6,8 +7,33 @@ namespace Eighth
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Введите кол-во вершин");
-            int n = Convert.ToInt32(Console.ReadLine());
+            Menu menu = new Menu(null);
+            Random random = new Random();
+            int n = menu.GetInt("кол-во вершин");
+            int[,] a_ = new int[n, n], b_ = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    a_[i, j] = random.Next(0, 10);
+                    b_[i, j] = random.Next(0, 10);
+                }
+            Matrice a = new Matrice(a_);
+            Matrice b = new Matrice(b_);
+
+            Matrice c = b;
+
+            Console.WriteLine("Генерация первой матрицы..");
+            a.Print();
+            Console.WriteLine("Генерация второй матрицы..");
+            b.Print();
+            Console.WriteLine("Генерация третьей матрицы, изоморфной к первой..");
+            c.Print();
+
+            Console.Write("Первая матрица изоморфна ко второй: ");
+            Console.WriteLine(Matrice.isIsomorph(a, b));
+            Console.Write("Первая матрица изоморфна к третьей: ");
+            Console.WriteLine(Matrice.isIsomorph(b, c));
 
         }
     }
